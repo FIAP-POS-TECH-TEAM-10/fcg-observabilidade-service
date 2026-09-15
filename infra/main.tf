ead8299bfcfdc41b7f5d4315ec1bdc0363e311d2
Eric Lopes Pedroso <ericlopespedroso@gmail.com>
2026-09-14 21:55:21 -0300

    Ajustes de infra 10

--- infra/main.tf
+++ infra/main.tf
@@ -67,18 +67,17 @@ resource "aws_security_group" "monitoring_sg" {
 
 # Instância EC2 (Recomendado t3.medium no mínimo para rodar toda essa stack)
 resource "aws_instance" "monitoring_server" {
-  ami                  = data.aws_ami.ubuntu.id
-  instance_type        = "t3.micro" # <--- Alterar de t2.micro para t3.micro
-  iam_instance_profile = aws_iam_instance_profile.ssm_profile.name
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro" # 100% elegível ao Free Tier (750h/mês)
+  
 
-  associate_public_ip_address = false
-  vpc_security_group_ids      = [aws_security_group.monitoring_sg_private.id]
+  vpc_security_group_ids = [aws_security_group.monitoring_sg.id]
 
   root_block_device {
-    volume_size = 30
+    volume_size = 30 # Limite gratuito de disco SSD (gp3)
     volume_type = "gp3"
   }
-  
+
   user_data = <<-EOF
               #!/bin/bash
               # 1. Configurar SWAP de 2GB para não estourar a memória da t2.micro
